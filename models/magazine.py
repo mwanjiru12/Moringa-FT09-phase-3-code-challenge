275d1a839557b31f260c160d4ab405daded24818
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-06-12 13:31:36 +0300

    "Refactored Magazine class: renamed variables, updated SQL queries, and renamed methods."

--- models/magazine.py
+++ models/magazine.py
@@ -1,27 +1,23 @@
-from database.connection import get_db_connection
-CONN = get_db_connection()
-CURSOR = CONN.cursor()
-
 class Magazine:
     def __init__(self, id, name, category):
-        self.id = id
-        self.name = name
-        self.category = category
+        self._id = id
+        self._name = name
+        self._category = category
 
     def save(self):
-        CURSOR.execute("SELECT id FROM magazines WHERE id = ?", (self._id,))
+        CURSOR.execute("SELECT id FROM magazines WHERE id =?", (self._id,))
         if CURSOR.fetchone():
-                raise ValueError(f"Article with id {self._id} already exists")
+            raise ValueError(f"Magazine with id {self._id} already exists")
         sql = """
          INSERT INTO magazines (
          id, name, category)  
-         VALUES (?, ?, ?)  
+         VALUES (?,?,?)  
         """
-        CURSOR.execute(sql,(self._id, self._name ,self._category))
+        CURSOR.execute(sql, (self._id, self._name, self._category))
         CONN.commit()
 
-    def find_cat_from_db(self, id):
-        sql = """SELECT category FROM magazines WHERE id = ?"""
+    def find_category_from_db(self, id):
+        sql = """SELECT category FROM magazines WHERE id =?"""
         CURSOR.execute(sql, (id,))
         row = CURSOR.fetchone()
         if row:
@@ -30,29 +26,27 @@ class Magazine:
         else:
             raise ValueError("The Category is not available in the database")
 
- 
-
-    def magazine_articles(self):
+    def get_magazine_articles(self):
         sql = """SELECT magazines.name, articles.id, articles.title FROM magazines
                 INNER JOIN articles ON magazines.id = articles.magazine_id
-                WHERE magazines.id = ?"""
+                WHERE magazines.id =?"""
         CURSOR.execute(sql, (self._id,))
         article_details = CURSOR.fetchall()
         return [row[2] for row in article_details]
 
-    def magazine_contributors(self):
+    def get_magazine_contributors(self):
         sql = """SELECT authors.id, authors.name, magazines.name FROM authors
                INNER JOIN articles ON authors.id = articles.author_id
                INNER JOIN magazines ON articles.magazine_id = magazines.id
-               WHERE magazines.id = ?"""
+               WHERE magazines.id =?"""
         CURSOR.execute(sql, (self._id,))
         contributors = CURSOR.fetchall()
         return [row[1] for row in contributors]
 
-    def article_titles(self):
+    def get_article_titles(self):
         sql = """SELECT magazines.name, articles.title FROM articles 
                 INNER JOIN magazines ON articles.magazine_id = magazines.id
-                WHERE magazines.id = ? """
+                WHERE magazines.id =? """
         CURSOR.execute(sql, (self._id,))
         titles = CURSOR.fetchall()
         if not titles:
@@ -60,13 +54,13 @@ class Magazine:
         else:
             return [row[1] for row in titles]
 
-    def contributing_authors(self):
+    def get_contributing_authors(self):
         sql = """
             SELECT authors.id, authors.name, COUNT(*) AS article_count
             FROM authors
             INNER JOIN articles ON authors.id = articles.author_id
             INNER JOIN magazines ON articles.magazine_id = magazines.id
-            WHERE magazines.id = ?
+            WHERE magazines.id =?
             GROUP BY authors.id, authors.name
             HAVING COUNT(*) > 2
         """
@@ -77,14 +71,6 @@ class Magazine:
             return None
         else:
             return [row[1] for row in authors_data]
+
     def __repr__(self):
         return f'<Magazine {self._name}>'
-
-
-# magazine1.add_mag_to_db()
-
-
-# print(magazine1.magazine_articles())
-# print(magazine1.magazine_contributors())
-# print(magazine1.article_titles())
-# print(magazine1.contributing_authors())
